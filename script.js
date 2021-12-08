c7558ab6c5a50ccfc996f2d513f1e18bef1b5bc3
skolotaja <94806629+skolotaja@users.noreply.github.com>
2021-12-08 23:38:34 +0200

    labojumi

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 let tabulasRindas = document.querySelector('.rindas');
 
 let ciparuSkaits;
-let variantuSkaits;
+let variantuSkaits=5;
 let papildusVards;
 
 const vardi1 = ['spalvains', 'karsta', 'spurains', 'mazs', 'samtains'];
@@ -15,21 +15,22 @@ function rand(maxCipars)
 }
 function veidotParoli()
 {
+  for (let i = 0; i <= variantuSkaits; i++) {
   let random1 = rand(vardi1.length);
   let random2 = rand(vardi2.length);
-  let random3 = rand(vardi2.length);
+  let random3 = rand(vardi3.length);
 
   //let random1 = rand(vardi1);
     //let random2 = rand(vardi2);
     //let random3 = rand(vardi3);
     //parole = vardi1[random1];
 
-    let paroleVards1 = vardi1[random1];
+    let parole1 = vardi1[random1];
     //paroleVards1 = paroleVards1.replace('i', '1');
-    let paroleVards2 = vardi1[random2];
-    let paroleVards3 = vardi1[random3];
+    let parole2 = vardi1[random2];
+    let parole3 = vardi1[random3];
 
-}
+
 //let parole=vardi1[0];
 
 //let parole1 = vardi1[1] + vardi2[1] + vardi3[1];
@@ -51,16 +52,17 @@ function veidotParoli()
 //variantuSkaits = document.querySelector('#variantuSkaits').value;
 //}
 //tabulasRindas.innerHTML+'';
-for (let i = 0; i <= variantuSkaits; i++) {
-  let parole = vardi1[i] + vardi2[i] + vardi3[i];
 
-  console.log(parole);
+  //let parole = vardi1[i] + vardi2[i] + vardi3[i];
+
+  //console.log(parole);
   tabulasRindas.innerHTML += `
           <tr>
-          <td>`+ (i + 1) + `</td>
-          <td>`+ parole + `</td>
+          <td>${i + 1}</td>
+          <td>$ {parole}</td>
           </tr>`;
 
+}
 }
 //}//cikla beigas
  //}
